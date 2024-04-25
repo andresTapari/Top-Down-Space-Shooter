78615ca27d0c43912fafc7af82af8067e95f2715
andresTapa <79794569+andresTapari@users.noreply.github.com>
2024-04-25 07:11:26 -0300

    fix enemy signal connecion
    LVL 7 intenta conectar una señal que tienen los enemigos, pero la instancia que se crea es una versión anterior que no la tiene.

--- scripts/singletone/definitions.gd
+++ scripts/singletone/definitions.gd
@@ -1,7 +1,7 @@
 extends Node
 
 ## Rutas de las escenas
-const ENEMY_PATH :   String = "res://scenes/03_enemy/enemy_03.tscn"
+const ENEMY_PATH :   String = "res://scenes/05_collision_n_signals/enemy_05.tscn"
 const PLAYER_BULLET: String = "res://scenes/04_laser_n_explosion/playerBullet.tscn"
 const ENEMY_BULLET:  String = "res://scenes/04_laser_n_explosion/enemyBullet.tscn"
 const EXPLOSION:     String = "res://scenes/04_laser_n_explosion/explosion.tscn"
